6a843bb74d4d48e8cba2b2464ec62a3a0e1bf304
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-03-21 11:48:08 +0530

    handle new line b/w numbers

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const add = (numberString) => {
     if (numberString === "") return 0;
 
-    const numberArray = numberString.split(",");
+    const normalizedNumbers = numberString.replace(/\n/g, ",");
+
+    const numberArray = normalizedNumbers.split(",");
+
     return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
 };
 
@@ -10,4 +13,5 @@ console.log(add(""));  //0
 console.log(add("1"));  //1
 console.log(add("1,5"));  //6
 console.log(add("1,5,6"));  //12 
-console.log(add("1,5,6,4"));  //16
+console.log(add("1,5,6,4"));  //16  
+console.log(add("1\n2,3"));  //6  
