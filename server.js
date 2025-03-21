9a617b4255e2f252b4743f566dc0093e0f746f2b
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-03-21 12:40:33 +0530

    handle multiple delimiters and custom length delimiters

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const add = (numberString) => {
     if (numberString === "") return 0;
 
-    if (numberString.startsWith("//")) {
-
-        const delimiterLineEnd = numberString.indexOf("\n");
-        const delimiter = numberString.slice(2, delimiterLineEnd);
-        const numbersList = numberString.slice(delimiterLineEnd + 1);
+    if (numberString.startsWith("//[")) {
 
 
-        const numberArray = numbersList.split(delimiter);
+        const delimiter = getDelimiter(numberString);
+        const formattedInput = formatInput(numberString);
+
+
+        const numberArray = getNumbers(formattedInput, delimiter);
         return sumNumbers(numberArray);
     }
 
@@ -18,6 +18,41 @@ const add = (numberString) => {
 
     return sumNumbers(numberArray);
 };
+
+function getNumbers(string, delimiter) {
+    return string.split(delimiter)
+        .filter(n => n !== '')
+        .map(n => parseInt(n));
+}
+
+function formatInput(input) {
+    const delimiterRegExp = /^(\/\/.*\n)/;
+    const matches = delimiterRegExp.exec(input);
+    if (matches && matches.length > 0) {
+        return input.replace(delimiterRegExp, '');
+    }
+
+    return input;
+}
+
+function getDelimiter(input) {
+    const delimiters = [];
+    const multipleDelimiterRegexp = /(?:^\/\/)?\[([^\[\]]+)\]\n?/g;
+    let matches = multipleDelimiterRegexp.exec(input);
+    while (matches !== null) {
+        delimiters.push(matches[1]);
+        matches = multipleDelimiterRegexp.exec(input);
+    }
+    if (delimiters.length > 0) {
+        return new RegExp('[' + delimiters.join('') + ']');
+    }
+    matches = /^\/\/(.*)\n/.exec(input);
+    if (matches && matches[1]) {
+        return matches[1];
+    }
+    return /[\n,]/;
+
+}
 
 function sumNumbers(numberArray) {
     let sum = 0, negatives = [];
@@ -31,6 +66,7 @@ function sumNumbers(numberArray) {
         }
         else {
             sum += number;
+
         }
     });
 
@@ -48,7 +84,8 @@ console.log(add("1,5"));  //6
 console.log(add("1,5,6"));  //12 
 console.log(add("1,5,6,4"));  //16  
 console.log(add("1\n2,3"));  //6  
-console.log(add("//;\n1;2"));  //3
+console.log(add("//;\n1;2"));  //3  
+console.log(add("//[***]\n1***2***3"));  //6
 console.log(add("2+1001"));  //2
 console.log(add("//;\n1;2;-1"));  //Negative numbers not allowed -1
 
