4bfca8fdb280c1513ad05510e4c6fce19c245364
PrabhudesaiNandan <nandan.prabhudesai@kilowott.com>
2025-03-21 11:58:18 +0530

    custom delimiter case

--- server.js
+++ server.js
@@ -1,12 +1,40 @@
 const add = (numberString) => {
     if (numberString === "") return 0;
 
+    if (numberString.startsWith("//")) {
+
+        const delimiterLineEnd = numberString.indexOf("\n");
+        const delimiter = numberString.slice(2, delimiterLineEnd);
+        const numbersList = numberString.slice(delimiterLineEnd + 1);
+
+
+        const numberArray = numbersList.split(delimiter);
+        return sumNumbers(numberArray);
+    }
+
     const normalizedNumbers = numberString.replace(/\n/g, ",");
 
     const numberArray = normalizedNumbers.split(",");
 
-    return numberArray.reduce((sum, num) => sum + parseInt(num), 0);
+    return sumNumbers(numberArray);
 };
+
+function sumNumbers(numberArray) {
+    let sum = 0;
+
+    numberArray.forEach(num => {
+        const number = parseInt(num);
+        if (isNaN(number)) return; // Ignore empty strings or invalid numbers
+
+        else {
+            sum += number;
+        }
+    });
+
+
+
+    return sum;
+}
 
 
 console.log(add(""));  //0
@@ -15,3 +43,4 @@ console.log(add("1,5"));  //6
 console.log(add("1,5,6"));  //12 
 console.log(add("1,5,6,4"));  //16  
 console.log(add("1\n2,3"));  //6  
+console.log(add("//;\n1;2"));  //3
